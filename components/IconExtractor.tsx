ad5bf3459a69bf1138ded4339d44f40b25addcaf
321208008 <321208008@qq.com>
2024-12-14 16:35:31 +0800

    fix bug

--- components/IconExtractor.tsx
+++ components/IconExtractor.tsx
@@ -148,7 +148,7 @@ export function IconExtractor({ language }: IconExtractorProps) {
                   <label className="text-sm font-medium">{t.format}</label>
                   <Select 
                     value={format} 
-                    onValueChange={setFormat}
+                    onValueChange={(value: string) => setFormat(value as IconFormat)}
                     disabled={isIcoSource && format === 'ico'}
                   >
                     <SelectTrigger>
